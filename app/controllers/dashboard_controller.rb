c709b574c5036ae35f9640ebfc422f4f695247c9
believemecha <mechathrust@gmail.com>
2025-07-04 23:15:34 +0530

    Add pagination and search for the file listing

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,8 +2,16 @@ class DashboardController < ApplicationController
     before_action :authenticate_user!
   
     def index
-      @file_uploads = current_user.file_uploads.order(created_at: :desc)
+        @file_uploads = current_user.file_uploads.order(created_at: :desc)
+
+        if params[:q].present?
+          query = "%#{params[:q].downcase}%"
+          @file_uploads = @file_uploads.where("LOWER(title) LIKE ? OR LOWER(description) LIKE ?", query, query)
+        end
+      
+        @file_uploads = @file_uploads.page(params[:page]).per(10) # Show 10 per page
     end
+      
   
     def show
     end
